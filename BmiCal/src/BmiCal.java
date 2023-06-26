f729175e136921d875cd8f2d7135d09d0e17c8d1
trashPandaFromAsia <arrogant10190@gmail.com>
2023-06-26 14:57:43 +0800

    Added bmiCategory method.

--- BmiCal/src/BmiCal.java
+++ BmiCal/src/BmiCal.java
@@ -17,8 +17,22 @@ public class BmiCal {
         double weight = varInput("weight", "kilogram");
         return weight / ((height / 100.0) * (height / 100.0));
     }
+
+    private static String bmiCategory(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal weight";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
     public static void bmiResult() {
         double bmi = calBmi();
         System.out.printf("Your BMI is: %.2f%n", bmi);
+        String bmiCategory = bmiCategory(bmi);
+        System.out.println("BMI Category: " + bmiCategory);
     }
 }
