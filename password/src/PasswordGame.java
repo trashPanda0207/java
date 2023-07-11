1e3e44c677647c537bf84f6c2ba82daaaff4cc08
trashPandaFromAsia <arrogant10190@gmail.com>
2023-07-11 10:19:26 +0800

    Changed file name and completed.

---
+++ password/src/PasswordGame.java
@@ -0,0 +1,57 @@
+import java.lang.Math;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PasswordGame {
+    public static void main(String[] args) {
+        starter();
+    }
+
+    public static void starter () {
+        game(randomNumber());
+    }
+
+    private static int randomNumber () {
+        int random = (int) (Math.random() * 100 + 1);
+        return random;
+    }
+
+    private static void game (int theNumber) {
+        int min = 0;
+        int max = 100;
+
+        while(true) {
+            int guess = guess(min, max);
+            if (theNumber == guess) {
+                System.out.println("You win.");
+                break;
+            } else if (theNumber > guess) {
+                min = guess;
+            } else if (theNumber < guess) {
+                max = guess;
+            }
+        }
+    }
+
+    private static int guess (int min, int max) {
+
+        while (true) {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Make a guess: (between " + min + " ~ " + max + "): ");
+
+            try {
+                int guess = s.nextInt();
+                if (guess < max && guess > min) {
+                    return guess;
+                }
+                else {
+                    System.out.println("Invalid number!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input!");
+                s.nextLine();
+            }
+
+        }
+    }
+}
