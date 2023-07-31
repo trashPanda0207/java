d703680454f6dfb8f3d160e5cdd0c1c4cb9db14a
trashPandaFromUbuntu <arrogant10190@gmail.com>
2023-07-31 21:06:57 +0800

    Completed fibonacci()

--- Exercises/src/PrintEvery3.java
+++ Exercises/src/PrintEvery3.java
@@ -220,6 +220,14 @@ public class PrintEvery3 {
         return finalIndex;
     }
 
+    private static int fibonacci(int n) {
+        if (n >= 3) {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        } else {
+            return 1;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
